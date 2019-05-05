1e3b5d8fba2f3c1239fc59a6c90d116b100f44c7
TildeHacker <contact@tildehacker.com>
2019-05-05 19:15:02 +0100

    Fix Windows filename error
    Fixes: #2
    Signed-off-by: TildeHacker <contact@tildehacker.com>

--- pijamo/__main__.py
+++ pijamo/__main__.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
+# Windows illegal characters
+# https://en.wikipedia.org/wiki/Filename
+# TODO: Collisions possible
+# TODO: May be slow
+def make_windows_compatible(path):
+    windows_illegal = [
+        '/',
+        '\\',
+        '?',
+        '%',
+        '*',
+        ':',
+        '|',
+        '"',
+        '<',
+        '>'
+    ]
+    for char in windows_illegal:
+        path = path.replace(char, '_')
+
+    return path
+
+
 try:
     import os
     import sys
@@ -24,16 +48,16 @@ try:
     # Destination
     dst_root_dir = sys.argv[2]
     dst_dir = os.path.join(
-                dst_root_dir,
-                albumartist[0].replace("/", "_"),
-                file_info['album'][0].replace("/", "_"))
+        dst_root_dir,
+        make_windows_compatible(albumartist[0]),
+        make_windows_compatible(file_info['album'][0]))
     dst_filename = ""
     dst_filename += str(discnumber) + "-"
     dst_filename += str(file_info['tracknumber'][0]).split("/")[0].zfill(2)\
         + " "
     dst_filename += str(file_info['title'][0]).replace("/", "_")\
         + os.path.splitext(src_file)[1]
-    dst_file = os.path.join(dst_dir, dst_filename)
+    dst_file = os.path.join(dst_dir, make_windows_compatible(dst_filename))
 
     # Same as `mkdir -p dst_dir`
     os.makedirs(dst_dir, exist_ok=True)
